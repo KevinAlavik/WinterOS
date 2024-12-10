aaa69b8a829494098cad0817dc4ee310bd0aa2d0
Kevin Alavik <kevin@alavik.se>
2024-12-10 17:09:16 +0100

    feat: Added init of video device

--- boot/boot.asm
+++ boot/boot.asm
@@ -7,9 +7,13 @@ org 0x10000
 ; Parameters passed from Firmware:
 ;  - r0 = Pointer to the WFPT
 entry:
+    ; Display boot message via the console
     mov r10, r0             ; Move the WFPT pointer to r10.
     mov r0, boot_msg        ; Set arg1 (r0) of the Print() function.
-    call [r10]              ; Call the print handler from the table, WFPT[0](boot_msg) = [WFPT + (0x08 * 0)] -> Print(boot_msg) 
+    call [r10]              ; Call the print handler from the table, WFPT[0](boot_msg) = [WFPT + (0x08 * 0)] -> Print(boot_msg)
+
+    ; Initialize the display
+    call [r10 + 0x08]       ; Call the video init from the table, WFTP[1]() = [WFTP + (0x08 * 1)] -> VideoInit()
     hlt
 
 ; Includes
