82774c9fcaf681dc24b7b1a77bb16734a19607d7
Kevin Alavik (Code Server) <kevin@alavik.se>
2024-12-10 22:11:36 +0100

    feat: Updated firmware

--- boot/boot.asm
+++ boot/boot.asm
@@ -10,11 +10,18 @@ entry:
     ; Display boot message via the console
     mov r10, r0             ; Move the WFPT pointer to r10.
     mov r0, boot_msg        ; Set arg1 (r0) of the Print() function.
+<<<<<<< HEAD
     call [r10]              ; Call the print handler from the table, WFPT[0](boot_msg) = [WFPT + (0x08 * 0)] -> Print(boot_msg)
 
     ; Initialize the display
     call [r10 + 0x08]       ; Call the video init from the table, WFTP[1]() = [WFTP + (0x08 * 1)] -> VideoInit()
+=======
+    mov r11, 0              ; Index 0
+    mov r12, 0x08           ; Offset 0x08
+    call [r12 * r11 + r10]  ; Call the print handler from the table, WFPT[0](boot_msg) = [offset * index + abase] -> Print(boot_msg)
+>>>>>>> eb9aacb (feat: Updated firmware)
     hlt
+
 
 ; Includes
 %include "utils/data.asm"
